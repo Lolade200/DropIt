6f7a4c08d43a0a976d7a599f3aabdb241c5cdf0b
Lolade200 <146335666+Lolade200@users.noreply.github.com>
2026-09-18 11:23:54 +0100

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,1 @@
-import React from 'react'
 
-function App() {
-  return (
-    <div className="app-container">
-      <h1>Earn With Grace</h1>
-      <iframe
-        src="https://earnwithgrace-61p7.onrender.com"
-        title="Earn With Grace Project"
-        style={{ width: '100%', height: '90vh', border: 'none' }}
-      />
-    </div>
-  )
-}
-
-export default App
